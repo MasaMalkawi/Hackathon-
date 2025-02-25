791227aec86d6f76f30383de09d28273340df8cf
MasaMalkawi <masamalkawi@gmail.com>
2025-02-25 15:04:39 +0300

    main menu

---
+++ Assets/_Project/Scripts/SceneSwitcher.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneSwitcher : MonoBehaviour
+{
+    public void LoadNextScene()
+    {
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(1); // Load the next scene (Change the index if needed)
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("Game is quitting..."); // This message helps in testing in the Unity Editor
+        Application.Quit(); // Closes the game (Works in a built app)
+    }
+}
