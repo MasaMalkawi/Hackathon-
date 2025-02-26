a6c0e7321e3d14c3e3064bc8420746d3d6566884
MasaMalkawi <masamalkawi@gmail.com>
2025-02-26 09:12:27 +0300

    trigger & audio

---
+++ Assets/_Project/Scripts/TriggerZone.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class TriggerZone : MonoBehaviour
+{
+    public AudioSource guideAudio;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("Player entered the trigger zone.");
+
+        if (other.CompareTag("Player"))
+        {
+            if (guideAudio != null && !guideAudio.isPlaying)
+            {
+                guideAudio.Play();
+                Debug.Log("Guide audio is playing.");
+            }
+        }
+    }
+}
